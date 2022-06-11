97148b017e2adda8413319479a52f918d73ae321
chris <chris@chriss-MacBook-Pro.local>
2022-06-11 10:14:59 -0700

    camnet and api update

--- resources/js/components/components/pages/initiatives/CamnetPage.jsx
+++ resources/js/components/components/pages/initiatives/CamnetPage.jsx
@@ -10,6 +10,9 @@ const CamnetPage = () => {
                 headline="CAMNET"
                 mainText="CAMNET is a network of web cameras intended to raise public awareness about the effects of air pollution on visibility. Typical visual range in the eastern U.S. is 15 to 30 miles, one-third of what it would be without man-made air pollution."
             />
+            <div style={{textAlign: 'center'}}>
+                <a href="https://hazecam.net/" target="_blank">CAMNET Website</a>
+            </div>
             <DocumentLinks docFilter="camnet"/>
             <ResourceLibraryLink docFilter="camnet"/>
         </>
