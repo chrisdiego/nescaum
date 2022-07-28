88bfd6612aa06f52f23c089a838d60ccd504d08d
chris <chrisdiego1@gmail.com>
2022-07-28 03:05:53 -0700

    mix

--- public/js/app.js
+++ public/js/app.js
@@ -25294,7 +25294,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "API_URL": () => (/* binding */ API_URL),
 /* harmony export */   "PageParams": () => (/* binding */ PageParams)
 /* harmony export */ });
-var API_URL = 'http://127.0.0.1:8000/api';
+var API_URL = 'http://159.223.119.211/api';
 var PageParams = {
   'PER_PAGE': 10
 };
